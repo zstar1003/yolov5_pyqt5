e6001de88dcc1a157c4d9c7b1fab001a981022f0
zstar <zstar1003@163.com>
2023-04-19 21:54:54 +0800

    适配YOLOv5最新版本，重构后端程序

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-## 项目概述
+## yolov5_pyqt5
 
-在项目<https://xugaoxiang.com/2021/06/30/yolov5-pyqt5>的基础上，精简了代码，增加了打开输出文件夹功能，美化了界面。
+这是一个使用pyqt5搭建YOLOv5目标检测可视化程序。
+
+项目采用了极简设计，以便读者可以基于此界面进行二次开发。
+
+主要功能：加载单图/选择模型/目标检测
+
+YOLOv5版本为最新版本(6.0+)，5.0版本可查看仓库Releases
+
+项目界面：
 
 ![image-20220821211811729](show.png)
 
+# 使用方式
+在使用本项目之前，建议先配置好最新版本的YOLOv5的环境并跑通
 
+之后在相同环境中安装pyqt5、qdarkstyle等依赖
 
-支持图片检测、视频检测、摄像头检测
+最后运行main.py即可看到显示界面
+ 
+## Stars over time
 
-博文详解：
+如果此项目对你有所帮助，请给项目点个star
 
-https://zstar.blog.csdn.net/article/details/126463741
+如果有任何问题，均可在此仓库中提出issue
+
+![Stargazers over time](https://starchart.cc/zstar1003/yolov5_pyqt5.svg)
--- main.py
+++ main.py
@@ -1,59 +1,30 @@
 import os
 import sys
-import cv2
-import random
-import torch
-import numpy as np
-import torch.backends.cudnn as cudnn
+import detect
 import qdarkstyle
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtGui import QIcon, QPixmap
-
-from utils.torch_utils import select_device
-from models.experimental import attempt_load
-from utils.general import check_img_size, non_max_suppression, scale_coords
-from utils.datasets import letterbox
-from utils.plots import plot_one_box
+from PyQt5.QtWidgets import QMessageBox
 
 
 class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(Ui_MainWindow, self).__init__(parent)
-        self.timer_video = QtCore.QTimer()
+        self.timer_detect = QtCore.QTimer()
         self.setupUi(self)
-        self.init_logo()
-        self.init_slots()
-        self.cap = cv2.VideoCapture()
-        self.out = None
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
-
-        cudnn.benchmark = True
-        weights = 'weights/yolov5s.pt'   # 模型加载路径
-        imgsz = 640  # 预测图尺寸大小
-        self.conf_thres = 0.25  # NMS置信度
-        self.iou_thres = 0.45  # IOU阈值
-
-        # 载入模型
-        self.model = attempt_load(weights, map_location=self.device)
-        stride = int(self.model.stride.max())
-        self.imgsz = check_img_size(imgsz, s=stride)
-        if self.half:
-            self.model.half()  # to FP16
-
-        # 从模型中获取各类别名称
-        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
-        # 给每一个类别初始化颜色
-        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
+        self.init_logo()  # 初始化logo
+        self.init_slots()  # 初始化槽函数
+        # 初始化属性
+        self.file_path = None
+        self.model_path = None
 
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(900, 600)
-        # MainWindow.setStyleSheet("")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        # self.centralwidget.setStyleSheet("border: 1px solid white;")
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.centralwidget)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
@@ -61,7 +32,6 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)  # 布局的左、上、右、下到窗体边缘的距离
-        # self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName("verticalLayout")
 
         # 打开图片按钮
@@ -75,38 +45,38 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.pushButton_img.setMaximumSize(QtCore.QSize(150, 40))
         font = QtGui.QFont()
         font.setFamily("Agency FB")
-        font.setPointSize(12)
+        font.setPointSize(14)
         self.pushButton_img.setFont(font)
         self.pushButton_img.setObjectName("pushButton_img")
         self.verticalLayout.addWidget(self.pushButton_img, 0, QtCore.Qt.AlignHCenter)
         self.verticalLayout.addStretch(5)  # 增加垂直盒子内部对象间距
 
-        # 打开摄像头按钮
-        self.pushButton_camera = QtWidgets.QPushButton(self.centralwidget)
+        # 加载模型按钮
+        self.pushButton_model = QtWidgets.QPushButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton_camera.sizePolicy().hasHeightForWidth())
-        self.pushButton_camera.setSizePolicy(sizePolicy)
-        self.pushButton_camera.setMinimumSize(QtCore.QSize(150, 40))
-        self.pushButton_camera.setMaximumSize(QtCore.QSize(150, 40))
-        self.pushButton_camera.setFont(font)
-        self.pushButton_camera.setObjectName("pushButton_camera")
-        self.verticalLayout.addWidget(self.pushButton_camera, 0, QtCore.Qt.AlignHCenter)
+        sizePolicy.setHeightForWidth(self.pushButton_model.sizePolicy().hasHeightForWidth())
+        self.pushButton_model.setSizePolicy(sizePolicy)
+        self.pushButton_model.setMinimumSize(QtCore.QSize(150, 40))
+        self.pushButton_model.setMaximumSize(QtCore.QSize(150, 40))
+        self.pushButton_model.setFont(font)
+        self.pushButton_model.setObjectName("pushButton_model")
+        self.verticalLayout.addWidget(self.pushButton_model, 0, QtCore.Qt.AlignHCenter)
         self.verticalLayout.addStretch(5)
 
-        # 打开视频按钮
-        self.pushButton_video = QtWidgets.QPushButton(self.centralwidget)
+        # 检测按钮
+        self.pushButton_detect = QtWidgets.QPushButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton_video.sizePolicy().hasHeightForWidth())
-        self.pushButton_video.setSizePolicy(sizePolicy)
-        self.pushButton_video.setMinimumSize(QtCore.QSize(150, 40))
-        self.pushButton_video.setMaximumSize(QtCore.QSize(150, 40))
-        self.pushButton_video.setFont(font)
-        self.pushButton_video.setObjectName("pushButton_video")
-        self.verticalLayout.addWidget(self.pushButton_video, 0, QtCore.Qt.AlignHCenter)
+        sizePolicy.setHeightForWidth(self.pushButton_detect.sizePolicy().hasHeightForWidth())
+        self.pushButton_detect.setSizePolicy(sizePolicy)
+        self.pushButton_detect.setMinimumSize(QtCore.QSize(150, 40))
+        self.pushButton_detect.setMaximumSize(QtCore.QSize(150, 40))
+        self.pushButton_detect.setFont(font)
+        self.pushButton_detect.setObjectName("pushButton_detect")
+        self.verticalLayout.addWidget(self.pushButton_detect, 0, QtCore.Qt.AlignHCenter)
         self.verticalLayout.addStretch(50)
 
         # 显示导出文件夹按钮
@@ -122,7 +92,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.pushButton_showdir.setObjectName("pushButton_showdir")
         self.verticalLayout.addWidget(self.pushButton_showdir, 0, QtCore.Qt.AlignHCenter)
 
-        # 右侧图片/视频填充区域
+        # 右侧图片填充区域
         self.verticalLayout.setStretch(2, 1)
         self.horizontalLayout.addLayout(self.verticalLayout)
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -131,7 +101,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.horizontalLayout.setStretch(0, 1)
         self.horizontalLayout.setStretch(1, 3)
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
-        self.label.setStyleSheet("border: 1px solid white;")  #  添加显示区域边框
+        self.label.setStyleSheet("border: 1px solid white;")  # 添加显示区域边框
 
         # 底部美化导航条
         MainWindow.setCentralWidget(self.centralwidget)
@@ -148,172 +118,98 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "YOLOv5目标检测平台"))
-        self.pushButton_img.setText(_translate("MainWindow", "图片检测"))
-        self.pushButton_camera.setText(_translate("MainWindow", "摄像头检测"))
-        self.pushButton_video.setText(_translate("MainWindow", "视频检测"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "YOLOv5目标检测系统"))
+        self.pushButton_img.setText(_translate("MainWindow", "选择图片"))
+        self.pushButton_model.setText(_translate("MainWindow", "选择模型"))
+        self.pushButton_detect.setText(_translate("MainWindow", "目标检测"))
         self.pushButton_showdir.setText(_translate("MainWindow", "打开输出文件夹"))
         self.label.setText(_translate("MainWindow", "TextLabel"))
 
     def init_slots(self):
-        self.pushButton_img.clicked.connect(self.button_image_open)
-        self.pushButton_video.clicked.connect(self.button_video_open)
-        self.pushButton_camera.clicked.connect(self.button_camera_open)
-        self.pushButton_showdir.clicked.connect(self.button_show_dir)
-        self.timer_video.timeout.connect(self.show_video_frame)
+        self.pushButton_img.clicked.connect(self.load_img)
+        self.pushButton_model.clicked.connect(self.select_model)
+        self.pushButton_detect.clicked.connect(self.target_detect)
+        self.pushButton_showdir.clicked.connect(self.show_dir)
 
     def init_logo(self):
-        pix = QtGui.QPixmap('')   # 绘制初始化图片
+        pix = QtGui.QPixmap('')  # 绘制初始化图片
         self.label.setScaledContents(True)
         self.label.setPixmap(pix)
 
-    def button_image_open(self):
+    def load_img(self):
         print('打开图片')
-        name_list = []
+        img_path, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self, "打开图片", "img", "All Files(*)")
+        print(img_path)
+        self.file_path = img_path
+        pixmap = QPixmap(img_path)
+        pixmap = pixmap.scaled(self.label.size(), Qt.IgnoreAspectRatio)
+        self.label.setPixmap(pixmap)
+        self.pushButton_img.setText(self.file_path.split('/')[-1])
 
-        img_name, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self, "打开图片", "", "*.jpg;;*.png;;All Files(*)")
-        if not img_name:
-            return
-        img = cv2.imread(img_name)
-        print(img_name)
-        showimg = img
-        with torch.no_grad():
-            img = letterbox(img, new_shape=self.imgsz)[0]
-            # Convert
-            # BGR to RGB, to 3x416x416
-            img = img[:, :, ::-1].transpose(2, 0, 1)
-            img = np.ascontiguousarray(img)
-            img = torch.from_numpy(img).to(self.device)
-            img = img.half() if self.half else img.float()  # uint8 to fp16/32
-            img /= 255.0  # 0 - 255 to 0.0 - 1.0
-            if img.ndimension() == 3:
-                img = img.unsqueeze(0)
-            # Inference
-            pred = self.model(img)[0]
-            # Apply NMS
-            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres)
-            # Process detections
-            for i, det in enumerate(pred):
-                if det is not None and len(det):
-                    # Rescale boxes from img_size to im0 size
-                    det[:, :4] = scale_coords(
-                        img.shape[2:], det[:, :4], showimg.shape).round()
+    # 选择模型
+    def select_model(self):
+        model_path = QtWidgets.QFileDialog.getOpenFileName(self, "选择模型", "weights", "Model files(*.pt)")
+        self.model_path = model_path
+        self.pushButton_model.setText(self.model_path[0].split('/')[-1])
 
-                    for *xyxy, conf, cls in reversed(det):
-                        label = '%s %.2f' % (self.names[int(cls)], conf)
-                        # print(label.split()[0])  # 打印各目标名称
-                        name_list.append(self.names[int(cls)])
-                        plot_one_box(xyxy, showimg, label=label,
-                                     color=self.colors[int(cls)], line_thickness=2)
+    # 目标检测
+    def target_detect(self):
+        detect_size = 640
+        if self.check_file():
+            # 点击之后防止误触，禁用按钮
+            self.pushButton_img.setEnabled(False)
+            self.pushButton_model.setEnabled(False)
+            self.pushButton_detect.setEnabled(False)
+            self.thread = DetectionThread(self.file_path, self.model_path, detect_size)
+            self.thread.start()
+            # 子线程运行结束之后signal_done，主线程执行UI更新操作
+            self.thread.signal_done.connect(self.flash_target)
 
-        cv2.imwrite('result/prediction.jpg', showimg)
-        self.result = cv2.cvtColor(showimg, cv2.COLOR_BGR2BGRA)
-        self.result = cv2.resize(self.result, (640, 480), interpolation=cv2.INTER_AREA)
-        self.QtImg = QtGui.QImage(self.result.data, self.result.shape[1], self.result.shape[0], QtGui.QImage.Format_RGB32)
-        self.label.setPixmap(QtGui.QPixmap.fromImage(self.QtImg))
+    # 目标检测之前检查是否选择了数据和模型
+    def check_file(self):
+        if self.file_path is None:
+            QMessageBox.information(self, '提示', '请先导入数据')
+            return False
+        if self.model_path is None:
+            QMessageBox.information(self, '提示', '请先选择模型')
+            return False
+        return True
 
-    def button_video_open(self):
-        video_name, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self, "打开视频", "", "*.mp4;;*.avi;;All Files(*)")
+    # 刷新
+    def flash_target(self):
+        img_path = os.getcwd() + '/result/' + [f for f in os.listdir('result')][0]
+        pixmap = QPixmap(img_path)
+        pixmap = pixmap.scaled(self.label.size(), Qt.IgnoreAspectRatio)
+        self.label.setPixmap(pixmap)
+        # 刷新完之后恢复按钮状态
+        self.pushButton_img.setEnabled(True)
+        self.pushButton_model.setEnabled(True)
+        self.pushButton_detect.setEnabled(True)
 
-        if not video_name:
-            return
-
-        flag = self.cap.open(video_name)
-        if flag == False:
-            QtWidgets.QMessageBox.warning(
-                self, u"Warning", u"打开视频失败", buttons=QtWidgets.QMessageBox.Ok, defaultButton=QtWidgets.QMessageBox.Ok)
-        else:
-            self.out = cv2.VideoWriter('result/vedio_prediction.avi', cv2.VideoWriter_fourcc(
-                *'MJPG'), 20, (int(self.cap.get(3)), int(self.cap.get(4))))
-            self.timer_video.start(30)
-            self.pushButton_video.setDisabled(True)
-            self.pushButton_img.setDisabled(True)
-            self.pushButton_camera.setDisabled(True)
-
-    def button_camera_open(self):
-        if not self.timer_video.isActive():
-            # 默认使用第一个本地camera
-            flag = self.cap.open(0)
-            if flag == False:
-                QtWidgets.QMessageBox.warning(
-                    self, u"Warning", u"打开摄像头失败", buttons=QtWidgets.QMessageBox.Ok, defaultButton=QtWidgets.QMessageBox.Ok)
-            else:
-                self.out = cv2.VideoWriter('result/camera_prediction.avi', cv2.VideoWriter_fourcc(
-                    *'MJPG'), 20, (int(self.cap.get(3)), int(self.cap.get(4))))
-                self.timer_video.start(30)
-                self.pushButton_video.setDisabled(True)
-                self.pushButton_img.setDisabled(True)
-                self.pushButton_camera.setText(u"关闭摄像头")
-        else:
-            self.timer_video.stop()
-            self.cap.release()
-            self.out.release()
-            self.label.clear()
-            self.init_logo()
-            self.pushButton_video.setDisabled(False)
-            self.pushButton_img.setDisabled(False)
-            self.pushButton_camera.setText(u"摄像头检测")
-
-    def show_video_frame(self):
-        name_list = []
-
-        flag, img = self.cap.read()
-        if img is not None:
-            showimg = img
-            with torch.no_grad():
-                img = letterbox(img, new_shape=self.imgsz)[0]
-                # Convert
-                # BGR to RGB, to 3x416x416
-                img = img[:, :, ::-1].transpose(2, 0, 1)
-                img = np.ascontiguousarray(img)
-                img = torch.from_numpy(img).to(self.device)
-                img = img.half() if self.half else img.float()  # uint8 to fp16/32
-                img /= 255.0  # 0 - 255 to 0.0 - 1.0
-                if img.ndimension() == 3:
-                    img = img.unsqueeze(0)
-                # Inference
-                pred = self.model(img)[0]
-
-                # Apply NMS
-                pred = non_max_suppression(pred, self.conf_thres, self.iou_thres)
-                # Process detections
-                for i, det in enumerate(pred):  # detections per image
-                    if det is not None and len(det):
-                        # Rescale boxes from img_size to im0 size
-                        det[:, :4] = scale_coords(
-                            img.shape[2:], det[:, :4], showimg.shape).round()
-                        # Write results
-                        for *xyxy, conf, cls in reversed(det):
-                            label = '%s %.2f' % (self.names[int(cls)], conf)
-                            name_list.append(self.names[int(cls)])
-                            # print(label)  # 打印各目标+置信度
-                            plot_one_box(
-                                xyxy, showimg, label=label, color=self.colors[int(cls)], line_thickness=2)
-
-            self.out.write(showimg)
-            show = cv2.resize(showimg, (640, 480))
-            self.result = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
-            showImage = QtGui.QImage(self.result.data, self.result.shape[1], self.result.shape[0],
-                                     QtGui.QImage.Format_RGB888)
-            self.label.setPixmap(QtGui.QPixmap.fromImage(showImage))
-
-        else:
-            self.timer_video.stop()
-            self.cap.release()
-            self.out.release()
-            self.label.clear()
-            self.pushButton_video.setDisabled(False)
-            self.pushButton_img.setDisabled(False)
-            self.pushButton_camera.setDisabled(False)
-            self.init_logo()
-
-    def button_show_dir(self):
-        path = os.getcwd() + '\\' + 'result'
+    # 显示输出文件夹
+    def show_dir(self):
+        path = os.getcwd() + '/' + 'result'
         os.system(f"start explorer {path}")
 
 
+# DetectionThread子线程用来执行目标检测
+class DetectionThread(QThread):
+    signal_done = pyqtSignal(int)  # 是否结束信号
+
+    def __init__(self, file_path, model_path, detect_size):
+        super(DetectionThread, self).__init__()
+        self.file_path = file_path
+        self.model_path = model_path
+        self.detect_size = detect_size
+        self.process = 0
+        self.total = 0
+
+    def run(self):
+        # 目标检测
+        detect.run(source=self.file_path, weights=self.model_path[0],
+                   imgsz=(self.detect_size, self.detect_size))
+        self.signal_done.emit(1)  # 发送结束信号
 
 
 if __name__ == '__main__':
