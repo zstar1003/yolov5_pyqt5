40c408c251c9f9804523d063c0214de8f2960677
zstar1003 <1198768105@qq.com>
2024-03-11 11:07:29 +0800

    定制功能修改

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2
 import detect
 import qdarkstyle
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QSize
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QMessageBox
 
@@ -18,6 +18,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(Ui_MainWindow, self).__init__(parent)
         self.timer_detect = QtCore.QTimer()
+        self.radioButtons = []
         self.setupUi(self)
         self.init_logo()  # 初始化logo
         self.init_slots()  # 初始化槽函数
@@ -26,6 +27,7 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.file_suffix = None  # 文件后缀
         self.result_path = "result"  # 检测图片保存路径
         self.init_file()  # 初始化必要的文件夹
+
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -41,22 +43,22 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)  # 布局的左、上、右、下到窗体边缘的距离
         self.verticalLayout.setObjectName("verticalLayout")
 
-        # 打开图片按钮
+        # 导入图片按钮
         self.pushButton_img = QtWidgets.QPushButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushButton_img.sizePolicy().hasHeightForWidth())
         self.pushButton_img.setSizePolicy(sizePolicy)
-        self.pushButton_img.setMinimumSize(QtCore.QSize(150, 40))
-        self.pushButton_img.setMaximumSize(QtCore.QSize(150, 40))
+        self.pushButton_img.setMinimumSize(QtCore.QSize(250, 80))
+        self.pushButton_img.setMaximumSize(QtCore.QSize(250, 80))
         font = QtGui.QFont()
         font.setFamily("Agency FB")
-        font.setPointSize(14)
+        font.setPointSize(24)
         self.pushButton_img.setFont(font)
         self.pushButton_img.setObjectName("pushButton_img")
         self.verticalLayout.addWidget(self.pushButton_img, 0, QtCore.Qt.AlignHCenter)
-        self.verticalLayout.addStretch(5)  # 增加垂直盒子内部对象间距
+        self.verticalLayout.addStretch(1)  # 增加垂直盒子内部对象间距
 
         # 加载模型按钮
         self.pushButton_model = QtWidgets.QPushButton(self.centralwidget)
@@ -65,12 +67,26 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushButton_model.sizePolicy().hasHeightForWidth())
         self.pushButton_model.setSizePolicy(sizePolicy)
-        self.pushButton_model.setMinimumSize(QtCore.QSize(150, 40))
-        self.pushButton_model.setMaximumSize(QtCore.QSize(150, 40))
+        self.pushButton_model.setMinimumSize(QtCore.QSize(250, 80))
+        self.pushButton_model.setMaximumSize(QtCore.QSize(250, 80))
         self.pushButton_model.setFont(font)
         self.pushButton_model.setObjectName("pushButton_model")
         self.verticalLayout.addWidget(self.pushButton_model, 0, QtCore.Qt.AlignHCenter)
-        self.verticalLayout.addStretch(5)
+        self.verticalLayout.addStretch(1)
+
+        # 融合按钮
+        self.pushButton_merge = QtWidgets.QPushButton(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_merge.sizePolicy().hasHeightForWidth())
+        self.pushButton_merge.setSizePolicy(sizePolicy)
+        self.pushButton_merge.setMinimumSize(QtCore.QSize(250, 80))
+        self.pushButton_merge.setMaximumSize(QtCore.QSize(250, 80))
+        self.pushButton_merge.setFont(font)
+        self.pushButton_merge.setObjectName("pushButton_merge")
+        self.verticalLayout.addWidget(self.pushButton_merge, 0, QtCore.Qt.AlignHCenter)
+        self.verticalLayout.addStretch(1)
 
         # 检测按钮
         self.pushButton_detect = QtWidgets.QPushButton(self.centralwidget)
@@ -79,27 +95,48 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushButton_detect.sizePolicy().hasHeightForWidth())
         self.pushButton_detect.setSizePolicy(sizePolicy)
-        self.pushButton_detect.setMinimumSize(QtCore.QSize(150, 40))
-        self.pushButton_detect.setMaximumSize(QtCore.QSize(150, 40))
+        self.pushButton_detect.setMinimumSize(QtCore.QSize(250, 80))
+        self.pushButton_detect.setMaximumSize(QtCore.QSize(250, 80))
         self.pushButton_detect.setFont(font)
         self.pushButton_detect.setObjectName("pushButton_detect")
         self.verticalLayout.addWidget(self.pushButton_detect, 0, QtCore.Qt.AlignHCenter)
-        self.verticalLayout.addStretch(5)
+        self.verticalLayout.addStretch(1)
 
-        # 视频检测按钮
-        self.pushButton_camera_detect = QtWidgets.QPushButton(self.centralwidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton_camera_detect.sizePolicy().hasHeightForWidth())
-        self.pushButton_camera_detect.setSizePolicy(sizePolicy)
-        self.pushButton_camera_detect.setMinimumSize(QtCore.QSize(150, 40))
-        self.pushButton_camera_detect.setMaximumSize(QtCore.QSize(150, 40))
-        self.pushButton_camera_detect.setFont(font)
-        self.pushButton_camera_detect.setObjectName("pushButton_camera_detect")
-        self.verticalLayout.addWidget(self.pushButton_camera_detect, 0, QtCore.Qt.AlignHCenter)
-        self.verticalLayout.addStretch(50)
+        # 切换按钮区域的布局
+        self.toggleButtonLayout = QtWidgets.QGridLayout()
+        # 定义按钮文本
+        button_texts = ["可见光图像", "红外图像", "融合图像", "可见光检测", "红外检测", "融合检测"]
+        # 创建六个小圆点切换按钮 (一列六行)，每个旁边有文字标记
+        for i, text in enumerate(button_texts):
+            # 创建单选按钮，并设置文本
+            radioButton = QtWidgets.QRadioButton(text, self.centralwidget)
+            self.radioButtons.append(radioButton)  # 添加到列
+            # 应用样式表调整大小和增加padding
+            radioButton.setStyleSheet("""
+                 QRadioButton {
+                     font-size: 20px; /* 调整字体大小 */
+                     min-height: 50px; /* 最小高度 */
+                     min-width: 120px; /* 最小宽度，包括文字和按钮 */
+                     padding: 10px; /* 内边距 */
+                     padding-left: 10px; /* 增加左边距以增强视觉效果 */
+                     padding-right: 10px; /* 增加右边距以增强视觉效果 */
+                 }
+                 QRadioButton::indicator {
+                     width: 20px; /* 单选按钮的圆点大小 */
+                     height: 20px;
+                 }
+             """)
+            # 将单选按钮添加到布局中，位置计算为：行 = i, 列 = 0
+            if i > 1:
+                radioButton.setEnabled(False)  # 禁用索引大于1的按钮
+            self.toggleButtonLayout.addWidget(radioButton, i, 0, 1, 1, QtCore.Qt.AlignHCenter)
 
+        # 为了使按钮组居中，我们在按钮组上方和下方添加拉伸因子
+        self.toggleButtonLayout.setRowStretch(0, 1)  # 在第一行之前添加一个拉伸因子
+        self.toggleButtonLayout.setRowStretch(len(button_texts) + 1, 1)  # 在最后一行之后添加一个拉伸因子
+        # 将切换按钮区域的布局添加到verticalLayout中
+        self.verticalLayout.addLayout(self.toggleButtonLayout)
+        self.verticalLayout.addStretch(10)
         # 显示导出文件夹按钮
         self.pushButton_showdir = QtWidgets.QPushButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
@@ -107,8 +144,8 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushButton_showdir.sizePolicy().hasHeightForWidth())
         self.pushButton_showdir.setSizePolicy(sizePolicy)
-        self.pushButton_showdir.setMinimumSize(QtCore.QSize(150, 50))
-        self.pushButton_showdir.setMaximumSize(QtCore.QSize(150, 50))
+        self.pushButton_showdir.setMinimumSize(QtCore.QSize(250, 80))
+        self.pushButton_showdir.setMaximumSize(QtCore.QSize(250, 80))
         self.pushButton_showdir.setFont(font)
         self.pushButton_showdir.setObjectName("pushButton_showdir")
         self.verticalLayout.addWidget(self.pushButton_showdir, 0, QtCore.Qt.AlignHCenter)
@@ -120,9 +157,12 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
         self.horizontalLayout.setStretch(0, 1)
-        self.horizontalLayout.setStretch(1, 3)
+        self.horizontalLayout.setStretch(1, 5)
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
         self.label.setStyleSheet("border: 1px solid white;")  # 添加显示区域边框
+        self.label.setScaledContents(True)
+        self.label.setMinimumSize(QSize(1571, 963))
+        self.label.setMaximumSize(QSize(1571, 963))
 
         # 底部美化导航条
         MainWindow.setCentralWidget(self.centralwidget)
@@ -133,17 +173,16 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "YOLOv5目标检测系统"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "RayNet图像融合检测系统"))
         self.pushButton_img.setText(_translate("MainWindow", "加载数据"))
         self.pushButton_model.setText(_translate("MainWindow", "选择模型"))
+        self.pushButton_merge.setText(_translate("MainWindow", "图像融合"))
         self.pushButton_detect.setText(_translate("MainWindow", "目标检测"))
-        self.pushButton_camera_detect.setText(_translate("MainWindow", "摄像头检测"))
         self.pushButton_showdir.setText(_translate("MainWindow", "打开输出文件夹"))
         self.label.setText(_translate("MainWindow", "TextLabel"))
 
@@ -153,7 +192,11 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.pushButton_model.clicked.connect(self.select_model)
         self.pushButton_detect.clicked.connect(self.target_detect)
         self.pushButton_showdir.clicked.connect(self.show_dir)
-        self.pushButton_camera_detect.clicked.connect(self.camera_detect)
+        self.pushButton_merge.clicked.connect(self.merge_enable)
+        # 绑定QRadioButton的信号
+        for radioButton in self.radioButtons:
+            radioButton.toggled.connect(self.update_image_display)
+
 
     # 绘制LOGO
     def init_logo(self):
@@ -188,17 +231,87 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
             self.pushButton_model.setText(self.model_path[0].split('/')[-1])
 
     # 目标检测
+    # def target_detect(self):
+    #     if self.check_file():
+    #         # 点击之后防止误触，禁用按钮
+    #         self.pushButton_img.setEnabled(False)
+    #         self.pushButton_model.setEnabled(False)
+    #         self.pushButton_detect.setEnabled(False)
+    #         self.thread = DetectionThread(self.file_path, self.model_path, self.label)
+    #         self.thread.start()
+    #         # 子线程运行结束之后signal_done，主线程执行UI更新操作
+    #         self.thread.signal_done.connect(self.flash_target)
+
+    def update_image_display(self):
+        if not self.file_path:
+            return
+        base_path, ext = os.path.splitext(self.file_path)
+        # 检查哪个按钮被选中，并更新label显示对应的图片
+        # 红外图像选中
+        if self.radioButtons[1].isChecked():
+            self.label.clear()
+            infrared_path = f"{base_path}_inf{ext}"
+            if os.path.exists(infrared_path):
+                pixmap = QPixmap(infrared_path)
+                scaled_pixmap = pixmap.scaled(self.label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                self.label.setPixmap(scaled_pixmap)
+            else:
+                QMessageBox.warning(self, '警告', f'红外图像不存在: {infrared_path}')
+        # 融合图像
+        elif self.radioButtons[2].isChecked():
+            self.label.clear()
+            infrared_path = f"{base_path}_merge{ext}"
+            if os.path.exists(infrared_path):
+                pixmap = QPixmap(infrared_path)
+                scaled_pixmap = pixmap.scaled(self.label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                self.label.setPixmap(scaled_pixmap)
+            else:
+                QMessageBox.warning(self, '警告', f'融合图像不存在: {infrared_path}')
+        # 可见光检测
+        elif self.radioButtons[3].isChecked():
+            self.label.clear()
+            infrared_path = f"{base_path}_detect{ext}"
+            if os.path.exists(infrared_path):
+                pixmap = QPixmap(infrared_path)
+                self.label.setPixmap(pixmap.scaled(self.label.size(), Qt.IgnoreAspectRatio))
+            else:
+                QMessageBox.warning(self, '警告', f'可见光检测图像不存在: {infrared_path}')
+        # 红外检测
+        elif self.radioButtons[4].isChecked():
+            self.label.clear()
+            infrared_path = f"{base_path}_inf_detect{ext}"
+            if os.path.exists(infrared_path):
+                pixmap = QPixmap(infrared_path)
+                self.label.setPixmap(pixmap.scaled(self.label.size(), Qt.IgnoreAspectRatio))
+            else:
+                QMessageBox.warning(self, '警告', f'红外检测图像不存在: {infrared_path}')
+        # 融合检测
+        elif self.radioButtons[5].isChecked():
+            self.label.clear()
+            infrared_path = f"{base_path}_merge_detect{ext}"
+            if os.path.exists(infrared_path):
+                pixmap = QPixmap(infrared_path)
+                self.label.setPixmap(pixmap.scaled(self.label.size(), Qt.IgnoreAspectRatio))
+            else:
+                QMessageBox.warning(self, '警告', f'融合图像检测不存在: {infrared_path}')
+        # 可见光
+        elif self.radioButtons[0].isChecked():
+            self.label.clear()
+            infrared_path = f"{base_path}{ext}"
+            if os.path.exists(infrared_path):
+                pixmap = QPixmap(infrared_path)
+                self.label.setPixmap(pixmap.scaled(self.label.size(), Qt.IgnoreAspectRatio))
+            else:
+                QMessageBox.warning(self, '警告', f'可见光图像不存在: {infrared_path}')
+
+    def merge_enable(self):
+        self.radioButtons[2].setEnabled(True)
+
+
     def target_detect(self):
-        if self.check_file():
-            # 点击之后防止误触，禁用按钮
-            self.pushButton_img.setEnabled(False)
-            self.pushButton_model.setEnabled(False)
-            self.pushButton_detect.setEnabled(False)
-            self.pushButton_camera_detect.setEnabled(False)
-            self.thread = DetectionThread(self.file_path, self.model_path, self.label)
-            self.thread.start()
-            # 子线程运行结束之后signal_done，主线程执行UI更新操作
-            self.thread.signal_done.connect(self.flash_target)
+        for i in range(3, 6):
+            self.radioButtons[i].setEnabled(True)
+
 
     # 目标检测之前检查是否选择了数据和模型
     def check_file(self):
@@ -228,32 +341,11 @@ class Ui_MainWindow(QtWidgets.QMainWindow):
         self.pushButton_img.setEnabled(True)
         self.pushButton_model.setEnabled(True)
         self.pushButton_detect.setEnabled(True)
-        self.pushButton_camera_detect.setEnabled(True)
 
     # 显示输出文件夹
     def show_dir(self):
         path = os.path.join(os.getcwd(), 'result')
         os.system(f"start explorer {path}")
-
-    # 摄像头检测
-    def camera_detect(self):
-        if Globals.camera_running:
-            Globals.camera_running = False
-            self.pushButton_camera_detect.setText("摄像头检测")
-            self.pushButton_img.setEnabled(True)
-            self.pushButton_model.setEnabled(True)
-            self.pushButton_detect.setEnabled(True)
-            self.label.clear()
-        else:
-            if self.check_model():
-                Globals.camera_running = True
-                self.pushButton_img.setEnabled(False)
-                self.pushButton_model.setEnabled(False)
-                self.pushButton_detect.setEnabled(False)
-                self.pushButton_camera_detect.setText("关闭摄像头")
-                self.camera_thread = CameraDetectionThread(self.model_path, self.label)
-                self.camera_thread.start()
-
 
 # DetectionThread子线程用来执行导入资源的目标检测
 class DetectionThread(QThread):
@@ -270,17 +362,6 @@ class DetectionThread(QThread):
         self.signal_done.emit(1)  # 发送结束信号
 
 
-# CameraDetectionThread子线程用来执行摄像头实时检测
-class CameraDetectionThread(QThread):
-    def __init__(self, model_path, label):
-        super(CameraDetectionThread, self).__init__()
-        self.model_path = model_path
-        self.label = label
-
-    def run(self):
-        detect.run(source=0, weights=self.model_path[0], show_label=self.label, save_img=False, use_camera=True)
-
-
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
@@ -293,5 +374,6 @@ if __name__ == '__main__':
     icon = QIcon()
     icon.addPixmap(QPixmap("./UI/icon.ico"), QIcon.Normal, QIcon.Off)
     ui.setWindowIcon(icon)
-    ui.show()
+    # ui.show()
+    ui.showMaximized()
     sys.exit(app.exec_())
